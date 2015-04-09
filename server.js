d498ac915d7b1839d991bff47b7cf746529b978a
rudy <347212291@qq.com>
2015-04-09 15:34:28 +0800

    add methodOverride for delete method

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var store = require('connect-mongo')(session);
+var methodOverride = require('method-override')
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(methodOverride())
 
 // app.set('trust proxy', 1); // trust first proxy
 app.use(session({
